7723874330c72f51143dea2db47d42e5ceb1cde0
Diego Massaneiro <dmassaneiro95@gmail.com>
2018-01-22 19:59:16 -0200

    ,

---
+++ conexao/ConexaoPDO.php
@@ -0,0 +1,23 @@
+  <?php
+   
+  class ConexaoPDO {
+   
+      public static $instance;
+   
+      private function __construct() {
+          //
+      }
+   
+      public static function getInstance() {
+          if (!isset(self::$instance)) {
+              self::$instance = new PDO('mysql:host=localhost;dbname=intranet', 'root', '39898890', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+              self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+          }
+   
+          return self::$instance;
+      }
+   
+  }
+   
+  
